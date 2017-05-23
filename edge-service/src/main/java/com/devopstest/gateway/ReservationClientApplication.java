dd714d54c37952f010446e129b3a0791296a72f7
anbu-vincent <anbu.vincent@randstadusa.com>
2017-05-22 22:38:41 -0400

    Edge-Service

---
+++ edge-service/src/main/java/com/devopstest/gateway/ReservationClientApplication.java
@@ -0,0 +1,37 @@
+package com.devopstest.gateway;
+
+import com.devopstest.jwt.DevOpsTestJWTConfig;
+import com.devopstest.log.LoggingAspect;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by demo
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
+@RefreshScope
+@EnableZuulProxy
+@EnableHystrix
+@Import({DevOpsTestJWTConfig.class, LoggingAspect.class})
+public class ReservationClientApplication {
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservationClientApplication.class, args);
+    }
+}
